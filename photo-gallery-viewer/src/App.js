49cad840c4e948d60a8f6ffd68356d8a897fec3a
oops-moment <prisha.kumar@students.iiit.ac.in>
2023-08-17 19:15:06 +0530

    added option to add images

--- photo-gallery-viewer/src/App.js
+++ photo-gallery-viewer/src/App.js
@@ -1,9 +1,13 @@
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import WSPGallery from "./components/WSPGallery";
 
 function App() {
-  const galleryImages = [
+  const [galleryImages, setGalleryImages] = useState([
+    {
+      img: "https://images.pexels.com/photos/1779487/pexels-photo-1779487.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
     {
       img: "https://images.pexels.com/photos/1779487/pexels-photo-1779487.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     },
@@ -22,9 +26,45 @@ function App() {
     {
       img: "https://images.pexels.com/photos/1712/sunglasses-apple-iphone-desk.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     },
-  ];
+    {
+      img: "https://images.pexels.com/photos/1779487/pexels-photo-1779487.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+      img: "https://images.pexels.com/photos/3861458/pexels-photo-3861458.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+      img: "https://images.pexels.com/photos/546819/pexels-photo-546819.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+      img: "https://images.pexels.com/photos/1194713/pexels-photo-1194713.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+      img: "https://images.pexels.com/photos/39284/macbook-apple-imac-computer-39284.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+      img: "https://images.pexels.com/photos/1712/sunglasses-apple-iphone-desk.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+  ]);
+
+  const [imageUrl, setImageUrl] = useState(""); // State to hold the input URL
+
+  const addImage = () => {
+    if (imageUrl) {
+      setGalleryImages((prevImages) => [...prevImages, { img: imageUrl }]);
+      setImageUrl(""); // Clear the input field after adding
+    }
+  };
+
   return (
     <div className="App">
+      <br />
+      <input
+        type="text"
+        placeholder="Enter image URL"
+        value={imageUrl}
+        onChange={(e) => setImageUrl(e.target.value)}
+      />
+      <button onClick={addImage}>Add Image</button>
       <br />
       <br />
       <br />
